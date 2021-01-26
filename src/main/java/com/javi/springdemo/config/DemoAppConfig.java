1c7fa16bdfdd4a9f5db806538557b22bceabbe6c
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-01-26 18:30:21 +0100

    set up all java configuration

---
+++ src/main/java/com/javi/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.javi.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.javi.springdemo")
+public class DemoAppConfig {
+
+}
