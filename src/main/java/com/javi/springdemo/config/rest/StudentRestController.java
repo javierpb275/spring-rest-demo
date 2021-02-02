96d4efdca407bc2a7670971376e93e4e071f6865
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-02-02 17:34:10 +0100

    refactored code in StudentRestController

--- src/main/java/com/javi/springdemo/config/rest/StudentRestController.java
+++ src/main/java/com/javi/springdemo/config/rest/StudentRestController.java
@@ -12,17 +12,25 @@ import com.javi.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	//define the @PostConstruct to load the student data only once
+	@PostConstruct
+	public void loadData() {
+		
+		theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Pepe", "Perez"));
+		theStudents.add(new Student("Paco", "Rodriguez"));
+		theStudents.add(new Student("Maria", "Garcia"));
+		
+	}
+	
 	//define endpoint for "/students" - return list of students
 	
 		@GetMapping("/students")
 		public List<Student> getStudents() {
-			
-			List<Student> theStudents = new ArrayList<>();
-			
-			theStudents.add(new Student("Pepe", "Perez"));
-			theStudents.add(new Student("Paco", "Rodriguez"));
-			theStudents.add(new Student("Maria", "Garcia"));
-			
 			return theStudents;
 		}
 	
