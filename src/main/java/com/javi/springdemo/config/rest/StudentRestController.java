368b8efbe31a4f02805a4b5f1177503b6d28aed4
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-02-02 17:16:50 +0100

    created StudentRestController

---
+++ src/main/java/com/javi/springdemo/config/rest/StudentRestController.java
@@ -0,0 +1,31 @@
+package com.javi.springdemo.config.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javi.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	//define endpoint for "/students" - return list of students
+	
+		@GetMapping("/students")
+		public List<Student> getStudents() {
+			
+			List<Student> theStudents = new ArrayList<>();
+			
+			theStudents.add(new Student("Pepe", "Perez"));
+			theStudents.add(new Student("Paco", "Rodriguez"));
+			theStudents.add(new Student("Maria", "Garcia"));
+			
+			return theStudents;
+		}
+	
+		//access the endpoint with /api/students
+
+}
